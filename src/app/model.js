840609600ace7d3c8fb0f335684ea83ac49a8dc3
owencwijaya <owencwijaya@gmail.com>
2023-02-20 16:15:57 +0700

    format: reformat model and rectangle code

--- src/app/model.js
+++ src/app/model.js
@@ -1,118 +1,124 @@
 class Model {
-    constructor(vertices = [], vertexCount = 0, type = "LINE", color = [0.0, 1.0, 0.0, 1.0], completed = false, count = 2) {
-        this.vertices = vertices;
-        this.vertexCount = vertexCount;
-        this.count = count;
-        this.type = type;
-        this.color = color;
-        this.completed = completed;
-        this.angle = 0;
-    }
+  constructor(
+    vertices = [],
+    vertexCount = 0,
+    type = "LINE",
+    color = [0.0, 1.0, 0.0, 1.0],
+    completed = false,
+    count = 2
+  ) {
+    this.vertices = vertices;
+    this.vertexCount = vertexCount;
+    this.count = count;
+    this.type = type;
+    this.color = color;
+    this.completed = completed;
+    this.angle = 0;
+  }
 
-    draw(x, y){
-        if (this.vertexCount < this.count) {
-            let vertex = {
-                id: 0,
-                position: [x, y],
-                color: this.color
-            }
-            
-            this.vertices.push(vertex);
-            this.vertices = ConvexHull(this.vertices)
-            this.vertexCount = this.vertices.length
+  draw(x, y) {
+    if (this.vertexCount < this.count) {
+      let vertex = {
+        id: 0,
+        position: [x, y],
+        color: this.color,
+      };
 
-            if (this.vertexCount == this.count){
-                enableAllButtons();
-                this.completed = true;
-            } 
+      this.vertices.push(vertex);
+      this.vertices = ConvexHull(this.vertices);
+      this.vertexCount = this.vertices.length;
 
-            renderAllObjects();
-        } 
-        renderObject(this);
-
-        for (var i = 0; i < this.vertexCount; i++){
-            this.vertices[i].id = i;
-        }
-    }
-
-    checkVertex(x, y){
-        // let index = 0;
-
-        for (const vertex of this.vertices) {
-            let point = getPoint(vertex.position[0], vertex.position[1]);
-
-            let topRightX = point.position[2];
-            let topRightY = point.position[3];
-            
-            let bottomLeftX = point.position[6];
-            let bottomLeftY = point.position[7];
-
-            let left = Math.min(topRightX, bottomLeftX);
-            let right = Math.max(topRightX, bottomLeftX);
-
-            let bottom = Math.min(topRightY, bottomLeftY);
-            let top = Math.max(topRightY, bottomLeftY);
-
-            if (x >= left && x <= right && y >= bottom && y <= top){
-                return vertex.id;
-            }
-            // index++;
-        }
-
-        return -1
-    }
-
-    isClicked(x, y){
-        if (this.completed){
-            let minX = this.vertices[0].position[0]
-            let minY = this.vertices[0].position[1]
-            let maxX = this.vertices[0].position[0]
-            let maxY = this.vertices[0].position[1]
-    
-            for (let i = 1; i < this.vertexCount; i++){
-                minX = Math.min(minX, this.vertices[i].position[0])
-                maxX = Math.max(maxX, this.vertices[i].position[0])
-                minY = Math.min(minY, this.vertices[i].position[1])
-                maxY = Math.max(maxY, this.vertices[i].position[1])
-            }
-    
-            return (minX <= x && x <= maxX && minY <= y && y <= maxY)
-        }
-    }
-
-    rotate(angle){
-        let DeltaAngle = angle - this.angle;
-        let xSum = 0;
-        let ySum = 0;
-
-        for (let i = 0; i < this.vertexCount; i++){
-            xSum += this.vertices[i].position[0];
-            ySum += this.vertices[i].position[1];
-        }
-        var xCenter = xSum / this.vertexCount;
-        var yCenter = ySum / this.vertexCount;
-        const rad = (Math.PI / 180) * DeltaAngle;
-        const cos = Math.cos(rad);
-        const sin = Math.sin(rad);
-
-        this.angle = angle;
-
-        for(let i = 0; i < this.vertexCount; i++){
-          var dx = this.vertices[i].position[0] - xCenter;
-          var dy = this.vertices[i].position[1] - yCenter;
-          this.vertices[i].position[0] = (dx*cos) + (dy*sin) + xCenter;
-          this.vertices[i].position[1] = (dy*cos) - (dx*sin) + yCenter;
-        }
+      if (this.vertexCount == this.count) {
+        enableAllButtons();
+        this.completed = true;
       }
 
-    resize(idx,x,y){
-        for (let i = 0; i < this.vertexCount; i++){
-            if (this.vertices[i].id == idx){
-                this.vertices[i].position[0] = x;
-                this.vertices[i].position[1] = y;
-                break;
-            }
-        }
+      renderAllObjects();
     }
-}
+    renderObject(this);
 
+    for (var i = 0; i < this.vertexCount; i++) {
+      this.vertices[i].id = i;
+    }
+  }
+
+  checkVertex(x, y) {
+    // let index = 0;
+
+    for (const vertex of this.vertices) {
+      let point = getPoint(vertex.position[0], vertex.position[1]);
+
+      let topRightX = point.position[2];
+      let topRightY = point.position[3];
+
+      let bottomLeftX = point.position[6];
+      let bottomLeftY = point.position[7];
+
+      let left = Math.min(topRightX, bottomLeftX);
+      let right = Math.max(topRightX, bottomLeftX);
+
+      let bottom = Math.min(topRightY, bottomLeftY);
+      let top = Math.max(topRightY, bottomLeftY);
+
+      if (x >= left && x <= right && y >= bottom && y <= top) {
+        return vertex.id;
+      }
+      // index++;
+    }
+
+    return -1;
+  }
+
+  isClicked(x, y) {
+    if (this.completed) {
+      let minX = this.vertices[0].position[0];
+      let minY = this.vertices[0].position[1];
+      let maxX = this.vertices[0].position[0];
+      let maxY = this.vertices[0].position[1];
+
+      for (let i = 1; i < this.vertexCount; i++) {
+        minX = Math.min(minX, this.vertices[i].position[0]);
+        maxX = Math.max(maxX, this.vertices[i].position[0]);
+        minY = Math.min(minY, this.vertices[i].position[1]);
+        maxY = Math.max(maxY, this.vertices[i].position[1]);
+      }
+
+      return minX <= x && x <= maxX && minY <= y && y <= maxY;
+    }
+  }
+
+  rotate(angle) {
+    let DeltaAngle = angle - this.angle;
+    let xSum = 0;
+    let ySum = 0;
+
+    for (let i = 0; i < this.vertexCount; i++) {
+      xSum += this.vertices[i].position[0];
+      ySum += this.vertices[i].position[1];
+    }
+    var xCenter = xSum / this.vertexCount;
+    var yCenter = ySum / this.vertexCount;
+    const rad = (Math.PI / 180) * DeltaAngle;
+    const cos = Math.cos(rad);
+    const sin = Math.sin(rad);
+
+    this.angle = angle;
+
+    for (let i = 0; i < this.vertexCount; i++) {
+      var dx = this.vertices[i].position[0] - xCenter;
+      var dy = this.vertices[i].position[1] - yCenter;
+      this.vertices[i].position[0] = dx * cos + dy * sin + xCenter;
+      this.vertices[i].position[1] = dy * cos - dx * sin + yCenter;
+    }
+  }
+
+  resize(idx, x, y) {
+    for (let i = 0; i < this.vertexCount; i++) {
+      if (this.vertices[i].id == idx) {
+        this.vertices[i].position[0] = x;
+        this.vertices[i].position[1] = y;
+        break;
+      }
+    }
+  }
+}
--- src/app/rectangle.js
+++ src/app/rectangle.js
@@ -1,71 +1,83 @@
 class Rectangle extends Model {
-  constructor(){
-    super([],0,'RECT',[0.0, 1.0, 0.0, 1.0],false,2)
+  constructor() {
+    super([], 0, "RECT", [0.0, 1.0, 0.0, 1.0], false, 2);
   }
-  draw(x, y){
+  draw(x, y) {
     if (this.vertexCount < this.count) {
-        let vertex = {
-            id: this.vertexCount == 0 ? 0 : 2,
-            position: [x, y],
-            color: this.color
-        }
-        
-        this.vertices.push(vertex);
+      let vertex = {
+        id: this.vertexCount == 0 ? 0 : 2,
+        position: [x, y],
+        color: this.color,
+      };
 
-        this.vertexCount++;
+      this.vertices.push(vertex);
 
-        if (this.vertexCount == this.count){
-            this.completed = true;
-            enableAllButtons();
+      this.vertexCount++;
 
-            let v1 = {
-              id: 1,
-              position: [this.vertices[0]['position'][0], this.vertices[1]['position'][1]],
-              color: this.color
-            }
+      if (this.vertexCount == this.count) {
+        this.completed = true;
+        enableAllButtons();
 
-            let v2 = {
-              id: 3,
-              position: [this.vertices[1]['position'][0], this.vertices[0]['position'][1]],
-              color: this.color
-            }
-            
-            this.vertices.splice(1,0,v1);
-            this.vertices.push(v2);
-            this.vertexCount += 2;
-        } 
-        renderAllObjects();
+        let v1 = {
+          id: 1,
+          position: [
+            this.vertices[0]["position"][0],
+            this.vertices[1]["position"][1],
+          ],
+          color: this.color,
+        };
+
+        let v2 = {
+          id: 3,
+          position: [
+            this.vertices[1]["position"][0],
+            this.vertices[0]["position"][1],
+          ],
+          color: this.color,
+        };
+
+        this.vertices.splice(1, 0, v1);
+        this.vertices.push(v2);
+        this.vertexCount += 2;
+      }
+      renderAllObjects();
     }
   }
 
-  resize(idx,x,y){
-    let a = (idx+1) % 4;
-    let b = (idx+2) % 4;
-    let c = (idx+3) % 4;
+  resize(idx, x, y) {
+    let a = (idx + 1) % 4;
+    let b = (idx + 2) % 4;
+    let c = (idx + 3) % 4;
 
-    let theta = this.angle * Math.PI / 180
+    let theta = (this.angle * Math.PI) / 180;
 
     const sin = Math.sin(theta);
     const cos = Math.cos(theta);
-    
+
     let deltaX = x - this.vertices[b].position[0];
     let deltaY = y - this.vertices[b].position[1];
 
     let length = deltaY * sin - deltaX * cos;
-    let width  = deltaY * cos + deltaX * sin;
+    let width = deltaY * cos + deltaX * sin;
 
     let lengthProyeksiX = length * Math.cos(theta);
     let lengthProyeksiY = length * Math.sin(theta);
     let widthProyeksiX = width * Math.cos(theta);
     let widthProyeksiY = width * Math.sin(theta);
 
-    this.vertices[idx].position[0] = this.vertices[b].position[0] - lengthProyeksiX + widthProyeksiY;
-    this.vertices[idx].position[1] = this.vertices[b].position[1] + lengthProyeksiY + widthProyeksiX;
+    this.vertices[idx].position[0] =
+      this.vertices[b].position[0] - lengthProyeksiX + widthProyeksiY;
+    this.vertices[idx].position[1] =
+      this.vertices[b].position[1] + lengthProyeksiY + widthProyeksiX;
 
-    this.vertices[a].position[0] = this.vertices[b].position[0] + widthProyeksiY;
-    this.vertices[a].position[1] = this.vertices[b].position[1] + widthProyeksiX;
+    this.vertices[a].position[0] =
+      this.vertices[b].position[0] + widthProyeksiY;
+    this.vertices[a].position[1] =
+      this.vertices[b].position[1] + widthProyeksiX;
 
-    this.vertices[c].position[0] = this.vertices[b].position[0] - lengthProyeksiX;
-    this.vertices[c].position[1] = this.vertices[b].position[1] + lengthProyeksiY;
+    this.vertices[c].position[0] =
+      this.vertices[b].position[0] - lengthProyeksiX;
+    this.vertices[c].position[1] =
+      this.vertices[b].position[1] + lengthProyeksiY;
   }
 }
